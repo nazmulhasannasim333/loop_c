e8619dcab46705776ed5294598f89f76177763d0
Nasim <mdnasimhosen333@gmail.com>
2025-04-11 00:45:51 +0600

    loop with condition

---
+++ loop_with_condition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("Even number: %d\n", i);
+        }
+        else
+        {
+            printf("Odd number: %d\n", i);
+        }
+    }
+    return 0;
+}
